2e8802ef52080e47e9c5e2879db33c6ed53600e6
laigrant112 <179632789+LailaG112@users.noreply.github.com>
2025-10-22 16:25:14 -0400

    adding swamp

--- Main Game Project/Assets/Scripts/Play Button.cs
+++ Main Game Project/Assets/Scripts/Play Button.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+ using UnityEngine.UI;
 
-public class Swamp : MonoBehaviour
+public class PlayButton : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +15,10 @@ public class Swamp : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+    
+    }
+    void OnMouseDown() {
+        SceneManager.LoadScene("Level 1");
+        Debug.Log("Object clicked!");
     }
 }
